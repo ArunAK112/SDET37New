9214eb8114de08d7a4aace72a12e3e54caac78e6
NishchithaNithin <109577712+NishchithaNithin@users.noreply.github.com>
2022-09-01 14:36:46 +0530

    Update AddProject.java

--- src/test/java/rmgyantra/AddProject.java
+++ src/test/java/rmgyantra/AddProject.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import com.mysql.cj.jdbc.Driver;
 
 public class AddProject {
-
+//CHANGES MADE BY NISHCHITHA
 	public static void main(String[] args) throws SQLException {
 		
 		Driver driverRef;
